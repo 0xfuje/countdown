11fe572f78ea19a9340ecbb1123c9b7032652850
Bence <blockchaineddev@gmail.com>
2021-09-15 16:14:13 +0200

    Countdown timer can now start from time input, resume and pause. Next steps: refactoring, scss styling, making a progress bar component

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CounterForm from './CounterForm';
 
 class Counter extends Component {
     constructor(props) {
@@ -7,27 +8,32 @@ class Counter extends Component {
             currentDate: '',
             endDate: '',
             timeLeft: '',
+            startIntervalID: '',
+            isClockRunning: true
         }
+        this.startCountdown = this.startCountdown.bind(this);
+        this.pauseCountdown = this.pauseCountdown.bind(this);
+        this.resumeCountdown = this.resumeCountdown.bind(this);
     }
     componentDidMount() {
-        
-        this.tick = setInterval(() => {
-            const currentMS = new Date().getTime();
-            const endMS = new Date(2021, 8, 15, 11).getTime();
-            const nCurrentDate = this.dateConverter(currentMS);
-            const nEndDate = this.dateConverter(endMS);
-            const nTimeLeft = this.dateConverter(endMS - currentMS);
-
-            this.setState({ 
-                currentDate: nCurrentDate,
-                endDate: nEndDate,
-                timeLeft: nTimeLeft
-            });
-        }, 990)
-        
+        const startDate = new Date().getTime() + 60*60*1000;
+        console.log(startDate);
+        const startIntervalID = setInterval(() => this.tick(startDate), 1000);
+        this.setState({ startIntervalID: startIntervalID });
     }
     componentWillUnmount() {
-        clearInterval(this.currentDate);
+        clearInterval(this.state.startIntervalID);
+    }
+    tick(endMS) {
+        const currentMS = new Date().getTime();
+        const nCurrentDate = this.dateConverter(currentMS);
+        const nEndDate = this.dateConverter(endMS);
+        const nTimeLeft = this.dateConverter(endMS - currentMS);
+        this.setState({ 
+            currentDate: nCurrentDate,
+            endDate: nEndDate,
+            timeLeft: nTimeLeft
+        });
     }
     dateConverter(date) {
         const parsedDate = new Date(date);
@@ -36,9 +42,40 @@ class Counter extends Component {
         const hours = Math.floor((parsedDate / 1000 / 60 / 60) % 24).toString().padStart(2, '0');
         return (`${hours}:${minutes}:${seconds}`)
     }
+    startCountdown(h, min) {
+        clearInterval(this.state.startIntervalID);
+        const currentMS = new Date().getTime();
+        const hourToMS = h * 60 * 60 * 1000;
+        const minToMS = min * 60 * 1000;
+        const totalMS = currentMS + (hourToMS + minToMS);
+        const startInterval = setInterval(() => this.tick(totalMS), 1000);
+        this.setState({ startIntervalID: startInterval, isClockRunning: true })
+    }
+    pauseCountdown() {
+        this.setState({ isClockRunning: false });
+        clearInterval(this.state.startIntervalID);
+    }
+    resumeCountdown() {
+        clearInterval(this.state.startIntervalID);
+        const [ hour, min, sec ] = this.state.timeLeft.split(':');
+        const currentMS = new Date().getTime();
+        const hourToMS = hour * 60 * 60 * 1000;
+        const minToMS = min * 60 * 1000;
+        const secToMS = sec * 1000;
+        const totalMS = currentMS + (hourToMS + minToMS + secToMS);
+        const startInterval = setInterval(() => this.tick(totalMS), 1000);
+        this.setState({ startIntervalID: startInterval, isClockRunning: true })
+    }
     render() {
         return (
             <div className='Counter'>
+                <CounterForm 
+                    startCountdown={this.startCountdown} 
+                    pauseCountdown={this.pauseCountdown}
+                    resumeCountdown={this.resumeCountdown}
+                    isClockRunning={this.state.isClockRunning}
+                    timeLeft={this.state.timeLeft}
+                />
                 <h1>{this.state.timeLeft}</h1>
             </div>
         )
--- src/CounterForm.js
+++ src/CounterForm.js
@@ -5,19 +5,55 @@ class CounterForm extends Component {
         super(props)
     
         this.state = {
-             hour: '',
-             minute: ''
+            hours: '',
+            minutes: '',
+            seconds: ''
         }
+        this.handleStart = this.handleStart.bind(this);
+        this.handleResume = this.handleResume.bind(this);
+        this.handlePause = this.handlePause.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
-    
+    handleResume(e) {
+        e.preventDefault();
+        this.setState({
+            hours: '',
+            minutes: ''
+        })
+        this.props.resumeCountdown();
+    }
+    handleStart(e) {
+        e.preventDefault();
+        this.props.startCountdown(this.state.hours, this.state.minutes);
+        this.setState({
+            hours: '',
+            minutes: ''
+        })
+    }
+    handleChange(e) {
+        e.preventDefault();
+        this.setState({[e.target.name] : e.target.value})
+    }
+    handlePause(e) {
+        e.preventDefault();
+        this.props.pauseCountdown();
+    }
+
     render() {
+        console.log(this.props.isClockRunning);
         return (
             <form className='CounterForm'>
-                <input name='hour' type="number" />
-                <input name='minute' type="number" />
+                <input name='hours' onChange={this.handleChange} className='CounterForm-hour' type="number" />
+                : 
+                <input name='minutes' onChange={this.handleChange} className='CounterForm-minute' type="number" />
+                <button onClick={this.handleStart}>Start</button>
+                {( this.props.isClockRunning
+                    ? <button onClick={this.handlePause}>Pause</button>
+                    : <button onClick={this.handleResume}>Resume</button>
+                      )}
             </form>
         )
     }
 }
 
-export default CounterForm
+export default CounterForm;
